9e35d57a30c241efc7a8b860476a11b09012996e
AE <efteka@usi.ch>
2021-05-21 18:36:22 +0200

    update

--- SQUIC_Py.py
+++ SQUIC_Py.py
@@ -23,26 +23,16 @@ def set_path(libSQUIC_path):
 		print(e)
 		return False
 
-
-def run(Y_learn,Y_test,l,M,W0,X0,max_iter,term_tol,drop_tol,verbose):	
+def SQUIC(Y, l, max_iter=100, drop_tol=1e-3, term_tol=1e-3,verbose=1, M=None, X0=None, W0=None):	
 
 	# if mode = [0,1,2,3,4] we Block-SQUIC or [5,6,7,8,9] Scalar-SQUIC
 	mode = c_int(0)
 
-	p,n1= Y_learn.shape
+	p,n= Y.shape
 
 	# The data needs to be fortran (column major)
-	Y_learn=np.array(Y_learn,order='F')
-	Y1   = Y_learn.ctypes.data_as(POINTER(c_double))
-
-	if(Y_test==None):
-		Y2   = POINTER(c_double)()
-		n2   = 0
-	else:
-		n2     = Y_test.shape[1]
-		Y_test = np.array(Y_learn,order='F')
-		Y2     = Y_test.ctypes.data_as(POINTER(c_double))
-		
+	Y=np.array(Y,order='F')
+	Y_ptr   = Y.ctypes.data_as(POINTER(c_double))
 
 	#################################################
 	# Matricis
@@ -94,44 +84,43 @@ def run(Y_learn,Y_test,l,M,W0,X0,max_iter,term_tol,drop_tol,verbose):
 	#################################################
 	# Parameters
 	#################################################
-	max_iter  = c_int(max_iter);
-	term_tol  = c_double(term_tol);
-	drop_tol  = c_double(drop_tol);
-	verbose   = c_int(verbose)
+	max_iter_ptr  = c_int(max_iter);
+	term_tol_ptr  = c_double(term_tol);
+	drop_tol_ptr  = c_double(drop_tol);
+	verbose_ptr   = c_int(verbose)
 
-	p    = c_long(p)
-	n1   = c_long(n1)
-	n2   = c_long(n2)
-	l    = c_double(l)
+	p_ptr    = c_long(p)
+	n_ptr    = c_long(n)
+	l_ptr    = c_double(l)
 
 	#################################################
 	# Information output buffers
 	#################################################
-	info_num_iter    = c_int(-1);
-	info_dgap        = c_double(-1);
-	info_logdetX_Y1  = c_double(-1);
-	info_trXS_Y2     = c_double(-1);
+	info_num_iter_ptr    = c_int(-1);
+	info_logdetX_ptr     = c_double(-1);
+	info_trSX_ptr        = c_double(-1);
 
-	info_times = POINTER(c_double)()
-	dll.SQUIC_CPP_UTIL_memset_double(byref(info_times),c_int(6))
+	info_times_ptr = POINTER(c_double)()
+	dll.SQUIC_CPP_UTIL_memset_double(byref(info_times_ptr),c_int(6))
 
-	info_objective = POINTER(c_double)()
-	dll.SQUIC_CPP_UTIL_memset_double(byref(info_objective),max_iter)
+	info_objective_ptr = POINTER(c_double)()
+	dll.SQUIC_CPP_UTIL_memset_double(byref(info_objective_ptr),max_iter_ptr)
 
 	#################################################
 	# Run SQUIC
 	#################################################
-	dll.SQUIC_CPP(mode,p,n1,Y1,n2,Y2, l, 
+	dll.SQUIC_CPP(mode,
+		p_ptr, n_ptr, Y_ptr,
+		l_ptr, 
 		M_rinx, M_cptr, M_val, M_nnz,
-		max_iter, drop_tol, term_tol, verbose,
-		byref(X_rinx),byref(X_cptr),byref(X_val),byref(X_nnz),
+		max_iter_ptr, drop_tol_ptr, term_tol_ptr, verbose_ptr,
+		byref(X_rinx), byref(X_cptr), byref(X_val), byref(X_nnz),
 		byref(W_rinx), byref(W_cptr), byref(W_val), byref(W_nnz),
-		byref(info_num_iter),
-		byref(info_times),	   # length must be 6: [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte]
-		byref(info_objective), # length must be size max_iter
-		byref(info_dgap),
-		byref(info_logdetX_Y1),
-		byref(info_trXS_Y2))
+		byref(info_num_iter_ptr),
+		byref(info_times_ptr),	   # length must be 6: [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte]
+		byref(info_objective_ptr), # length must be size max_iter
+		byref(info_logdetX_ptr),
+		byref(info_trSX_ptr))
 
 
 	#################################################
@@ -139,13 +128,12 @@ def run(Y_learn,Y_test,l,M,W0,X0,max_iter,term_tol,drop_tol,verbose):
 	#################################################
 	
 	#Convert all scalars values back python
-	p               = p.value
+	p               = p_ptr.value
 	X_nnz           = X_nnz.value
 	W_nnz           = W_nnz.value
-	info_num_iter   = info_num_iter.value
-	info_dgap       = info_dgap.value
-	info_logdetX_Y1 = info_logdetX_Y1.value
-	info_trXS_Y2    = info_trXS_Y2.value
+	info_num_iter   = info_num_iter_ptr.value
+	info_logdetX    = info_logdetX_ptr.value
+	info_trSX       = info_trSX_ptr.value
 
 	# Transfer Matrix from C to Python
 	# First we link the C buffer to a numpy array, than we make CSR matrix using a Copy of the array.
@@ -160,12 +148,11 @@ def run(Y_learn,Y_test,l,M,W0,X0,max_iter,term_tol,drop_tol,verbose):
 	W_val_py	=np.ctypeslib.as_array(W_val,(W_nnz,))
 	W 			=csr_matrix((W_val_py, W_rinx_py, W_cptr_py),shape=(p, p),copy=True)
 
-	temp			=np.ctypeslib.as_array(info_objective,(info_num_iter,))
-	info_obj_trace	=np.array(temp,copy=True)
+	temp			=np.ctypeslib.as_array(info_objective_ptr,(info_num_iter,))
+	info_objective	=np.array(temp,copy=True)
 
-	temp			=np.ctypeslib.as_array(info_times,(6,))
-	info_time_list	=np.array(temp,copy=True)
-
+	temp			=np.ctypeslib.as_array(info_times_ptr,(6,))
+	info_times   	=np.array(temp,copy=True)
 
 	#################################################
 	# Transfer Restusl from C to Python
@@ -183,7 +170,15 @@ def run(Y_learn,Y_test,l,M,W0,X0,max_iter,term_tol,drop_tol,verbose):
 		dll.SQUIC_CPP_UTIL_memfree_integer(byref(M_cptr))
 		dll.SQUIC_CPP_UTIL_memfree_double(byref(M_val))
 
-	dll.SQUIC_CPP_UTIL_memfree_double(byref(info_objective))
-	dll.SQUIC_CPP_UTIL_memfree_double(byref(info_times))
+	dll.SQUIC_CPP_UTIL_memfree_double(byref(info_objective_ptr))
+	dll.SQUIC_CPP_UTIL_memfree_double(byref(info_times_ptr))
 
-	return [ X,W,info_obj_trace,info_time_list,info_logdetX_Y1,info_trXS_Y2,info_dgap]
+	return [X,W,info_times,info_objective,info_logdetX,info_trSX]
+
+def SQUIC_S(Y, l,verbose=1, M=None):
+
+	[_,S,info_times,_,_,_]=SQUIC(Y=Y, l=l, max_iter=0, verbose=verbose, M=M)
+
+	return [S,info_times]
+
+
--- squic_test.py
+++ squic_test.py
@@ -1,4 +1,4 @@
-import SQUIC_Py as SQUIC
+import SQUIC_Py as SQUIC_Py
 import numpy as np
 from scipy.sparse import csr_matrix,isspmatrix_csr, identity
 
@@ -12,17 +12,17 @@ def tridiag(off_diag, diag, p):
 np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
 
-p=10
-n=8
+p=20
+n=10
 
 
 
 np.random.seed(1)
 iC_star = tridiag(-0.5,1.25,p)
 L = np.linalg.cholesky(iC_star)
-Y1 = np.linalg.solve(L.T,np.random.randn(p,n))
+Y = np.linalg.solve(L.T,np.random.randn(p,n))
 
-S=np.cov(Y1,bias=1)
+S=np.cov(Y,bias=1)
 
 
 print("There is n=%d samples and p=%d variables"% (n,p));
@@ -32,46 +32,25 @@ if(n>p):
 print("True Inverse Covariance Matrix: \n",iC_star);
 
 # Set the SQUIC Library Path
-#SQUIC.set_path('/Users/aryan/gdrive/files/code/SQUIC_Release_Source/darwin20/libSQUIC.dylib')
-SQUIC.set_path('/local_home/aryan/SQUIC_Release_Source/linux/libSQUIC.so')
+SQUIC_Py.set_path('/Users/aryan/gdrive/files/code/SQUIC_Release_Source/darwin20/libSQUIC.dylib')
+#SQUIC.set_path('/local_home/aryan/SQUIC_Release_Source/linux/libSQUIC.so')
 
 
 
 # Scalar SQUIC Paramter Runtime
-Y_train = Y1
-Y_test  = None
-
-
-W0 = None
-X0 = None
-
-M = None
 l=.25
 
-max_itr=100
-term_tol=1e-3
-drop_tol=1e-3
-verbose=1
-
-[X,W,info_obj_trace,info_time_list,info_logdetX_Y1,info_trXS_Y2,info_dgap]=SQUIC.run(Y_train,Y_test,l,M,X0,W0,max_itr,term_tol,drop_tol,verbose)
+[X,W,info_times,info_objective,info_logdetX,info_trSX]=SQUIC_Py.SQUIC(Y=Y,l=l)
 print("Inverse Covariance Matrix \n", X.todense())
 print("Covariance Matrix \n", W.todense())
-print("Objective Function Trace\n", info_obj_trace)
-print("Time List [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte] \n", info_time_list)
-print("info_logdetX_Y1 \n", info_logdetX_Y1)
-print("info_trXS_Y2 \n", info_trXS_Y2)
-print("info_dgap \n", info_dgap)
+print("Objective Function Trace\n", info_objective)
+print("Time List [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte] \n", info_times)
+print("info_logdetX \n", info_logdetX)
+print("info_trSX \n", info_trSX)
 
 
 
 # Matrix SQUIC Paramter Runtime
-Y_train = Y1
-Y_test  = None
-
-
-W0 = None
-X0 = None
-
 # The M matrix forms the matrix penelty matrix. Let Lambda be matrix penelty parameter and l be scalar penelty paramter:
 # Than Lambda :=  M + l(1-pattern(M)). 
 # This mean M encodes as bias, the structure of the inverse covariance matrix with nonzero values equaling the 
@@ -93,19 +72,20 @@ M = M* 1e-6;
 
 l=.5
 
-max_itr=100
-term_tol=1e-3
-drop_tol=1e-3
-verbose=1
 
-[X,W,info_obj_trace,info_time_list,info_logdetX_Y1,info_trXS_Y2,info_dgap]=SQUIC.run(Y_train,Y_test,l,M,X0,W0,max_itr,term_tol,drop_tol,verbose)
+[X,W,info_times,info_objective,info_logdetX,info_trSX]=SQUIC_Py.SQUIC(Y=Y,l=l,M=M)
 print("Inverse Covariance Matrix \n", X.todense())
 print("Covariance Matrix \n", W.todense())
-print("Objective Function Trace\n", info_obj_trace)
-print("Time List [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte] \n", info_time_list)
-print("info_logdetX_Y1 \n", info_logdetX_Y1)
-print("info_trXS_Y2 \n", info_trXS_Y2)
-print("info_dgap \n", info_dgap)
+print("Objective Function Trace\n", info_objective)
+print("Time List [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte] \n", info_times)
+print("info_logdetX \n", info_logdetX)
+print("info_trSX \n", info_trSX)
+
+
+[S,info_times]=SQUIC_Py.SQUIC_S(Y=Y,l=l)
+print("Sample Covariance Matrix \n", S.todense())
+print("Time List [time_total,time_impcov,time_optimz,time_factor,time_aprinv,time_updte] \n", info_times)
+
 
 
 
